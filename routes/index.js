a778d00b51bb58ad39bb3fa004c3a03fef772aa2
Kamal Thapa <handsomelotus1@cmail.com>
2023-07-31 23:21:06 -0400

    My new express back end project

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const clothingItems = require("./clothingItems");
 const createProfile = require("./users");
-const handleError = require("../utils/config");
+const { ERROR_404 } = require("../utils/errors");
 
 router.use("/items", clothingItems);
 router.use("/users", createProfile);
 
 router.use((req, res) => {
-  handleError(req, res, err);
+  res.status(ERROR_404).send({ message: "The requested resource not found" });
 });
 
 module.exports = router;
